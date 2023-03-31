9e08c16e15aa2e2d0edb4837b6803371277a3eec
Pedram Arianmanesh <127370501+Pedram-Arianmanesh@users.noreply.github.com>
2023-03-31 20:03:47 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+   int a,b;
+   printf("Please enter your decimal base number:");
+   scanf("%d",&a);
+   printf("Now enter your wanted base from 2 to 9:");
+   scanf("%d",&b);
+   int i=0;
+   int x=a;
+   while (x>0){
+    x=x/b;
+    i++;
+   }
+  long int Nmab[i];
+   x=0;
+   while(a>0){
+   Nmab[x]=a%b;
+   a=a/b;
+   x++;
+   }
+    x=x-1;
+
+    while(i>0){
+   printf("%d",Nmab[x]);
+   i=i-1;
+   x=x-1;
+}
+
+
+    return 0;
+}
